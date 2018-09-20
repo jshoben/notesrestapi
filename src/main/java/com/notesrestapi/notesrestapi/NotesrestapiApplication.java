18a9a709b4bc1f3dd0b527ea0bade5aff028f738
jshoben <jshoben@gmail.com>
2018-09-19 22:45:41 -0500

    initial commit

---
+++ src/main/java/com/notesrestapi/notesrestapi/NotesrestapiApplication.java
@@ -0,0 +1,12 @@
+package com.notesrestapi.notesrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesrestapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/notesrestapi/notesrestapi/NotesrestapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.notesrestapi.notesrestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotesrestapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
