b0abe6d1bc0c72926c35c2487725b8698bb4cde6
jshoben <jshoben@gmail.com>
2018-09-20 01:41:37 -0500

    Updated controller mapping

--- src/main/java/com/notesrestapi/notesrestapi/controller/NoteController.java
+++ src/main/java/com/notesrestapi/notesrestapi/controller/NoteController.java
@@ -9,28 +9,23 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/notes")
 public class NoteController {
 
     @Autowired
     NoteService noteService;
 
-    @RequestMapping("/")
-    public String test() {
-        return "Test is success";
-    }
-
-    @RequestMapping(value = "/notes/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Note findNote(@PathVariable("id") int id) {
         return noteService.findNote(id);
     }
 
-    @RequestMapping(value = "/notes", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public List<Note> findAll(@RequestParam(value = "query", required = false) String query) {
         return noteService.findAll(query);
     }
 
-    @RequestMapping(value = "/notes", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public Note create(@RequestBody Map<String, String> newNote) {
         String body = newNote.get("body");
 
